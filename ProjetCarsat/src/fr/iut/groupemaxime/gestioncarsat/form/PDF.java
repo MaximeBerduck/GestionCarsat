f0ccecbd0f0e09835bf802fca710c0e74107c7d5
maxime <maxime@maximes-mbp>
2019-11-05 21:58:52 +0100

    Ajout de la classe PDF

---
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/form/PDF.java
@@ -0,0 +1,40 @@
+package fr.iut.groupemaxime.gestioncarsat.form;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+
+public class PDF {
+	private PDDocument modele;
+	private PDAcroForm formulaire;
+	private PDField champ;
+	
+	public PDF(File source) throws IOException {
+		modele = PDDocument.load(source);
+		formulaire = modele.getDocumentCatalog().getAcroForm();
+		champ = null;
+	}
+	
+	public void remplirChamp(String champ, String valeur) throws IOException {
+		this.champ = formulaire.getField(champ);
+		this.champ.setValue(valeur);
+	}
+	
+	public void sauvegarderPDF() throws IOException {
+		modele.save("target/PDF/Doc1modif.pdf");
+	}
+	
+	public void fermerPDF() throws IOException {
+		modele.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		PDF om = new PDF(new File("target/PDF/Doc1.pdf"));
+		om.remplirChamp("surname", "salut");
+		om.sauvegarderPDF();
+		om.fermerPDF();
+	}
+}
