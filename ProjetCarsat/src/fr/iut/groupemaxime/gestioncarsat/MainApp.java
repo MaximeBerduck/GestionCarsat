2cc8e4faf0348fe3d9dcadb01f5a9fa3de798ddb
maxime <maxime@fe80:0:0:0:64a8:4aff:fe7f:1f43%9>
2019-11-12 19:14:10 +0100

    Ajout d'un OM à la liste en fin de saisie OM

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/MainApp.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/MainApp.java
@@ -1,6 +1,19 @@
 package fr.iut.groupemaxime.gestioncarsat;
 
+import java.io.File;
 import java.io.IOException;
+
+import fr.iut.groupemaxime.gestioncarsat.model.Agent;
+import fr.iut.groupemaxime.gestioncarsat.view.MenuAgentController;
+import fr.iut.groupemaxime.gestioncarsat.model.Avion;
+import fr.iut.groupemaxime.gestioncarsat.model.ListeOrdreMission;
+import fr.iut.groupemaxime.gestioncarsat.model.Mission;
+import fr.iut.groupemaxime.gestioncarsat.model.MissionPermanent;
+import fr.iut.groupemaxime.gestioncarsat.model.MissionTemporaire;
+import fr.iut.groupemaxime.gestioncarsat.model.OrdreMission;
+import fr.iut.groupemaxime.gestioncarsat.model.Train;
+import fr.iut.groupemaxime.gestioncarsat.model.Transport;
+import fr.iut.groupemaxime.gestioncarsat.model.Voiture;
 import fr.iut.groupemaxime.gestioncarsat.view.AgentController;
 import fr.iut.groupemaxime.gestioncarsat.view.TransportController;
 import fr.iut.groupemaxime.gestioncarsat.view.MissionController;
@@ -21,26 +34,31 @@ public class MainApp extends Application {
 	private AgentController controllerAgent;
 	private MissionController controllerMission;
 	private TransportController controllerTransport;
+	private ListeOrdreMission listeOM;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("OM");
-		// this.primaryStage.isResizable(); --> propriété redimensionnement de la
-		// fenêtre
+		// this.primaryStage.isResizable(); --> propriï¿½tï¿½ redimensionnement de la
+		// fenï¿½tre
 		initialiseRootLayout();
 		afficherListOm();
-		afficherFormInfoPerso();
 
 	}
 
 	private void afficherListOm() {
+		listeOM = new ListeOrdreMission();
+		listeOM.chargerOM(new File("target/OM/"));
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/MenuAgent.fxml"));
 			AnchorPane page = loader.load();
 
 			rootLayout.setLeft(page);
+			
+			MenuAgentController controller = loader.getController();
+			controller.setMainApp(this);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -136,6 +154,82 @@ public class MainApp extends Application {
 	}
 
 	public void validerOrdreMission() {
-		//TODO
+		// TODO
+		Agent agent = new Agent(controllerAgent.getNomTextField().getText(),
+				controllerAgent.getPrenomTextField().getText(),
+				Integer.parseInt(controllerAgent.getNumCAPSSATextField().getText()),
+				controllerAgent.getFonctionTextField().getText(),
+				controllerAgent.getResidenceAdminTextField().getText(),
+				controllerAgent.getUniteTavailTextField().getText(),
+				Integer.parseInt(controllerAgent.getCoefficientTextField().getText()),
+				Integer.parseInt(controllerAgent.getCodeAnalytiqueTextField().getText()));
+		Mission mission;
+		if (controllerMission.getOrdrePermanentRadioBtn().isSelected()) {
+			mission = new MissionPermanent();
+		} else {
+			String dateDebut = String.valueOf(controllerMission.getDateDebut().getValue().getDayOfMonth()) + '/'
+					+ String.valueOf(controllerMission.getDateDebut().getValue().getMonth()) + '/'
+					+ String.valueOf(controllerMission.getDateDebut().getValue().getYear());
+			String dateFin = String.valueOf(controllerMission.getDateFin().getValue().getDayOfMonth()) + '/'
+					+ String.valueOf(controllerMission.getDateFin().getValue().getMonth()) + '/'
+					+ String.valueOf(controllerMission.getDateFin().getValue().getYear());
+			String titre;
+			if (controllerMission.getFonctionHabituelleRadioBtn().isSelected()) {
+				titre = "fonctionHabituelle";
+			} else {
+				titre = "formation";
+			}
+			mission = new MissionTemporaire(dateDebut, "12:00", dateFin, "12:00",
+					controllerMission.getMotifDeplacementTextField().getText(),
+					controllerMission.getLieuDeplacementTextField().getText(), titre);
+		}
+
+		Transport transport;
+
+		if (controllerTransport.getAvionRadioBtn().isSelected()) {
+			String cramco;
+			if (controllerTransport.getCramcoNonRadioBtn().isSelected()) {
+				cramco = "non";
+			} else {
+				cramco = "oui";
+			}
+			transport = new Avion(cramco);
+		}
+
+		else if (controllerTransport.getVoitureRadioBtn().isSelected()) {
+			String appartenanceVehicule;
+			if (controllerTransport.getVehiculePersoRadioBtn().isSelected()) {
+				appartenanceVehicule = "vehiculePerso";
+			} else {
+				appartenanceVehicule = "vehiculeService";
+			}
+			transport = new Voiture(controllerTransport.getTypeVoitureTextField().getText(),
+					controllerTransport.getImmatriculationTextField().getText(),
+					Integer.parseInt(controllerTransport.getNbrCVTextField().getText()), appartenanceVehicule);
+		}
+
+		else {
+			String classe;
+			if (controllerTransport.getTrain1ereClasseRadioBtn().isSelected()) {
+				classe = "premiereClasse";
+			} else {
+				classe = "deuxiemeClasse";
+			}
+
+			String cramco;
+			if (controllerTransport.getCramcoNonRadioBtn().isSelected()) {
+				cramco = "non";
+			} else {
+				cramco = "oui";
+			}
+			transport = new Train(classe, cramco);
+		}
+		
+		OrdreMission om = new OrdreMission(agent,mission,transport);
+		listeOM.ajouterOM(om);
+	}
+	
+	public void creerNouveauOm() {
+		afficherFormInfoPerso();
 	}
 }
