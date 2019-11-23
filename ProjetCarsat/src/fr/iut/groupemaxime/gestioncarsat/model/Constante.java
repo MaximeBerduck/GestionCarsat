e96d1a0f597e8058a520753842763ea033989b20
maxime <maxime@maximes-mbp>
2019-11-23 14:04:59 +0100

    Ajout fichiers d'options

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/model/Constante.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/model/Constante.java
@@ -1,9 +1,22 @@
 package fr.iut.groupemaxime.gestioncarsat.model;
 
+import java.util.HashMap;
+
 public class Constante {
 	public static String CHEMIN_OM = "target/OM/";
 	public static String CHEMIN_PDF = "target/PDF/";
 	public static String EXTENSION_XML = ".xml";
 	public static String EXTENSION_PDF = ".pdf";
 	public static String CHEMIN_PDF_VIDE = "target/PDF/OM_vide.pdf";
+
+	public static String[] CHAMPS_AGENT = { "nomPrenom", "numCAPSSA", "fonction", "residenceAdmin", "uniteTravail",
+			"codeAnalytique", "coefficient" };
+	private static HashMap<String,String> MAP_AGENT = new HashMap<String,String>();
+	static
+	{
+		MAP_AGENT.put("nomPrenom","toto");
+		MAP_AGENT.put("numCAPSSA","titi");
+	}
+	
+	public static String CHEMIN_OPTIONS = "target/options.json";
 }
---
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/model/Options.java
@@ -0,0 +1,74 @@
+package fr.iut.groupemaxime.gestioncarsat.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+import com.google.gson.Gson;
+
+public class Options {
+	String cheminOM;
+	
+	public Options(String cheminOM) {
+		this.cheminOM = cheminOM;
+	}
+	
+	public Options() {
+		this(null);
+	}
+	
+	public void sauvegarderOptions() {
+		Gson g = new Gson();
+		String s = g.toJson(this);
+		FileWriter f;
+		try {
+			f = new FileWriter(new File(Constante.CHEMIN_OPTIONS));
+			f.write(s);
+			f.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Options chargerOptions() {
+		Gson g = new Gson();
+		Options options = null;
+		InputStream is;
+        try {
+            is = new FileInputStream(new File(Constante.CHEMIN_OPTIONS));
+            // Creation du JsonReader depuis Json.
+            JsonReader reader = Json.createReader(is);
+            // Recuperer la structure JsonObject depuis le JsonReader.
+            JsonObject objetJson = reader.readObject();
+            reader.close();            
+            options = g.fromJson(objetJson.toString(), Options.class);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return options;
+	}
+	
+	public static boolean fichierExiste() {
+		boolean existe = false;
+		File f = new File(Constante.CHEMIN_OPTIONS);
+		if(f.isFile())
+		{ 
+			existe = true;
+		}
+		return existe;
+	}
+	
+	public String getCheminOM() {
+		return this.cheminOM;
+	}
+
+}
