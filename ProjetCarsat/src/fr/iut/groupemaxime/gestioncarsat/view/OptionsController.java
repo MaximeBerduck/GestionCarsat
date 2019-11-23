cc372ae141d3c32d5bb7b79ba2d47774e44521f8
maxime <maxime@maximes-mbp>
2019-11-23 18:39:25 +0100

    Ajout de la modification de l'agent dans les options

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/view/OptionsController.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/view/OptionsController.java
@@ -1,11 +1,15 @@
 package fr.iut.groupemaxime.gestioncarsat.view;
 
 import java.io.File;
+import java.io.IOException;
 
 import fr.iut.groupemaxime.gestioncarsat.MainApp;
 import fr.iut.groupemaxime.gestioncarsat.model.Options;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -15,6 +19,8 @@ public class OptionsController {
 	
 	private MainApp mainApp;
 	private Options options;
+	private Stage secondaryStage;
+	
 	@FXML
 	private void initialize() {
 
@@ -35,6 +41,28 @@ public class OptionsController {
 		this.mainApp = mainApp;
 		this.options = options;
 		this.labelCheminDossierOM.setText(this.options.getCheminOM());
+	}
+	
+	public void modifierAgent() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/AgentOptions.fxml"));
+			AnchorPane optionsLayout = loader.load();
+
+			Scene scene = new Scene(optionsLayout);
+			secondaryStage = new Stage();
+			AgentOptionsController agentOptionsController = loader.getController();
+			agentOptionsController.chargerPage(this, options);
+			agentOptionsController.setChamps(options.getAgent());
+			secondaryStage.setScene(scene);
+			secondaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void fermerSecondaryStage() {
+		this.secondaryStage.close();
 	}
 	
 	public void sauvegarder() {
