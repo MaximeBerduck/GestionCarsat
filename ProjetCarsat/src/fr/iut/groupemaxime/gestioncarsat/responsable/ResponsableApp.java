6e845ee37dc08bc38e4b2e5dee1cd0cfccdde16e
DEVEAU Mylan <mylan.deveau@etu.unilim.fr>
2019-12-10 17:05:15 +0100

    correction controler

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/responsable/ResponsableApp.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/responsable/ResponsableApp.java
@@ -1,13 +1,17 @@
 package fr.iut.groupemaxime.gestioncarsat.responsable;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ResponsableApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		VBox root = new VBox();
+		primaryStage.setScene(new Scene(root,400,400));
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
