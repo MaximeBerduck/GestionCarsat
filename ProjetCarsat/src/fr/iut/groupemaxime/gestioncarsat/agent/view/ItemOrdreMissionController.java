9cdb9a70c71090d5ac5db8050c5641fed847f6c7
maxime <maxime.berduck@etu.unilim.fr>
2019-12-13 14:27:16 +0100

    ajout bouton signer menu OM

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/agent/view/ItemOrdreMissionController.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/agent/view/ItemOrdreMissionController.java
@@ -9,12 +9,19 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import fr.iut.groupemaxime.gestioncarsat.agent.form.PDF;
+import fr.iut.groupemaxime.gestioncarsat.agent.model.Bibliotheque;
 import fr.iut.groupemaxime.gestioncarsat.agent.model.Constante;
 import fr.iut.groupemaxime.gestioncarsat.agent.model.MissionTemporaire;
 import fr.iut.groupemaxime.gestioncarsat.agent.model.OrdreMission;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class ItemOrdreMissionController {
@@ -24,6 +31,10 @@ public class ItemOrdreMissionController {
 	private Label datesLabel;
 	@FXML
 	private VBox boite;
+	@FXML
+	private Button boutonSigner;
+	@FXML
+	private Button boutonEnvoyer;
 
 	private OrdreMission om;
 
@@ -67,12 +78,74 @@ public class ItemOrdreMissionController {
 	public void afficherEnvoiMail(ActionEvent event) {
 		this.mainApp.afficherEnvoiDuMail();
 	}
+	
+	@FXML
+	public void signerOM() {
+		if (Bibliotheque.fichierExiste(this.mainApp.getOptions().getCheminSignature())) {
+			this.om.setSignatureAgent(true);
+			this.boutonSigner.setVisible(false);
+			this.boutonEnvoyer.setVisible(true);
+			this.om.sauvegarderJson(this.om.getCheminDossier());
+		}
+		else {
+			TextInputDialog dialog = new TextInputDialog("");
+			dialog.setTitle("Signature non renseignée");
+			dialog.setHeaderText("Vous n'avez pas encore renseigné le chemin vers votre signature.");
+			
+			GridPane pageAlert = new GridPane();
+			TextField tfCheminSignature = new TextField();
+			tfCheminSignature.setPromptText("Chemin vers votre signature");
+			
+			Button boutonCheminSignature = new Button();
+			boutonCheminSignature.setText("...");
+			boutonCheminSignature.setOnAction(new EventHandler<ActionEvent>() {
+				 
+			    @Override
+			    public void handle(ActionEvent event) {
+			    	File cheminSignature = Bibliotheque.ouvrirFileChooser(Constante.IMAGE_FILTER);
+			    	if(null != cheminSignature) {
+			    		tfCheminSignature.setText(cheminSignature.toString());
+			    	}
+			    }
+			});
+			
+			pageAlert.add(new Label("Chemin vers votre signature : "), 0, 0);
+			pageAlert.add(tfCheminSignature, 1, 0);
+			pageAlert.add(boutonCheminSignature,2,0);
+			
+			dialog.getDialogPane().setContent(pageAlert);
+			dialog.showAndWait();
+			
+			if("".equals(tfCheminSignature.getText())) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Erreur chemin signature");
+				alert.setHeaderText("Vous n'avez pas saisi l'adresse vers votre signature !");
+				alert.showAndWait();
+			}
+			else {
+				this.mainApp.getOptions().setCheminSignature(tfCheminSignature.getText());
+				this.mainApp.getOptions().sauvegarderJson(Constante.CHEMIN_OPTIONS);
+				this.om.setSignatureAgent(true);
+				this.boutonSigner.setVisible(false);
+				this.boutonEnvoyer.setVisible(true);
+				this.om.sauvegarderJson(this.mainApp.getOptions().getCheminOM());
+			}
+		}
+	}
 
 	public void chargerOM(OrdreMission om) {
 		this.om = om;
 		this.lieuLabel.setText(((MissionTemporaire) om.getMission()).getLieuDeplacement());
 		this.datesLabel.setText("Du " + ((MissionTemporaire) om.getMission()).getDateDebut() + " au "
 				+ ((MissionTemporaire) om.getMission()).getDateFin());
+		if(om.agentSigne()) {
+			this.boutonSigner.setVisible(false);
+			this.boutonEnvoyer.setVisible(true);
+		}
+		else {
+			this.boutonSigner.setVisible(true);
+			this.boutonEnvoyer.setVisible(false);
+		}
 
 	}
 
