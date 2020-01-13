a52e219828e9381d0686654a3a6c5852c7ba36a6
Gautier Mechaussie <49200160+Tyegoteks@users.noreply.github.com>
2020-01-13 15:11:50 +0100

    Horaire Modif

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/agent/view/HorairesTravailController.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/agent/view/HorairesTravailController.java
@@ -26,7 +26,6 @@ public class HorairesTravailController {
 	@FXML
 	private SplitPane horaireTravailSplit;
 	
-	//private Stage primaryStage;
 	private AgentApp agentApp;
 	private Options options;
 	private OrdreMission missionActive;
