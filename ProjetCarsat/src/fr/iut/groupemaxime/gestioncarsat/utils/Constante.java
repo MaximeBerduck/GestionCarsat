1b2133d75380e8bd1bd77ec888e80bebb903fe87
DEVEAU Mylan <mylan.deveau@etu.unilim.fr>
2020-01-14 14:42:21 +0100

    sauvegarde des mails non envoyé

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/utils/Constante.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/utils/Constante.java
@@ -9,6 +9,7 @@ public class Constante {
 	public static String CHEMIN_PDF = "target/PDF/";
 	public static String EXTENSION_XML = ".xml";
 	public static String EXTENSION_PDF = ".pdf";
+	public static String EXTENSION_MAIL = ".eml";
 	public static String CHEMIN_PDF_VIDE = "target/PDF/OM_vide.pdf";
 	public static String CHEMIN_FICHIERS_DEFAUT = "target/OM/";
 	public static String EXTENSION_JSON = ".json";
