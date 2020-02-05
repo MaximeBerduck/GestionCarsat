60c9262bdb7235e34c921d0d0de1497adb79f690
DEVEAU Mylan <mylan.deveau@etu.unilim.fr>
2020-02-05 21:59:12 +0100

    mail sur port 25

--- ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/mail/MailProcessor.java
+++ ProjetCarsat/src/fr/iut/groupemaxime/gestioncarsat/mail/MailProcessor.java
@@ -93,7 +93,7 @@ public class MailProcessor {
 
 		props.put("mail.smtp.host", Constante.HOSTNAME);
 
-		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.port", "25");
 
 		props.put("mail.smtp.auth", "true");
 
